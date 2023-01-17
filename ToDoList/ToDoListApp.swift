c970d52b56a09269e2d6fd09e1219ca87ca1ca02
Andrey <studenkoff@gmail.com>
2023-01-17 11:22:32 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Andrey Studenkov on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
